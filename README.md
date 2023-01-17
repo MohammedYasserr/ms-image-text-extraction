bf18e620d9a17dbf5ad170bb09a6fd71090a2e70
MohammedYasserr <bmbmyasser@gmail.com>
2023-01-17 21:49:01 +0200

    Updating the README file plus the setting up the Jinja2 Tempaltes

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ ML Django Microservice text extraction from images
 Description : This is an Django Application to extract text from images using OCR API
 
 Starting the application : uvicorn app.main:app --reload
+In order to install the requirements.txt file follow the following steps : 
+    1 - Activate the virtual Environmet 
+    2 - run "pip install -r requirements.txt"
--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
+import pathlib
 import json
-from fastapi import FastAPI 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+
+BASE_DIR = pathlib.Path(__file__).parent #the directory where the app is(This method  gives us where the parent path is)  
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates")) 
 
-
-@app.get('/') # http GET
-def home_view():
-    return {"hello" : "world"}
+@app.get('/' , response_class=HTMLResponse) # http GET
+def home_view(request : Request):
+    print(request)
+    return "<h1> Hello, World !</h1>"
 
 
 @app.post('/') # http POST
