c109431ad17e9bbc1abed6a89f88b5961b841b08
MohammedYasserr <bmbmyasser@gmail.com>
2023-01-17 23:08:29 +0200

    This main file was deleted for a reason, but I get it back

--- app/main.py
+++ app/main.py
@@ -5,17 +5,21 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 
-BASE_DIR = pathlib.Path(__file__).parent #the directory where the app is(This method  gives us where the parent path is)  
+# the directory where the app is(This method  gives us where the parent path is)
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
-templates = Jinja2Templates(directory=str(BASE_DIR / "templates")) 
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-@app.get('/' , response_class=HTMLResponse) # http GET
-def home_view(request : Request):
+
+@app.get('/', response_class=HTMLResponse)  # http GET
+def home_view(request: Request):
     print(request)
-    return templates.TemplateResponse("home.html" , {"request" :request })
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.post('/') # http POST
+@app.post('/')  # http POST
 def home_detail_view():
-    return{"hello": "post"}
+    return {"hello": "post"}
+
+
