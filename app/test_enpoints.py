138d81a16d22b02aedccb9b4e177981104c04770
MohammedYasserr <bmbmyasser@gmail.com>
2023-01-17 23:44:49 +0200

    testing the POST Endpoint

--- app/test_enpoints.py
+++ app/test_enpoints.py
@@ -4,6 +4,13 @@ from app.main import app
 client = TestClient(app) 
 
 
-def test_get_home():
+def test_get_home(): #Testing the GET Endpoint
     response = client.get("/")
     assert response.status_code == 200
+    assert "text/html" in response.headers['content-type']
+
+
+def test_post_home():
+    response = client.post("/")
+    assert response.status_code  == 200 
+    assert "application/json" in response.headers['content-type']
