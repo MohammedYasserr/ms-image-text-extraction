c705c87db617b330957dd44903943f857c39b8ff
MohammedYasserr <bmbmyasser@gmail.com>
2023-02-26 00:05:36 +0200

    changed some comments in the test file

--- app/test_enpoints.py
+++ app/test_enpoints.py
@@ -4,13 +4,15 @@ from app.main import app
 client = TestClient(app) 
 
 
-def test_get_home(): #Testing the GET Endpoint
+def test_get_home(): 
+    """"Testing the get endpoint""" 
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers['content-type']
 
 
 def test_post_home():
+    """Testing the post endpoint"""
     response = client.post("/")
     assert response.status_code  == 200 
     assert "application/json" in response.headers['content-type']
